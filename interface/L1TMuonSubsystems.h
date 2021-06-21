7223d4f6456bef986c07cbc5df636397e717d5c4
Jia Fu Low <jia.fu.low@cern.ch>
2021-06-20 23:21:17 -0400

    Create L1TMuonSubsystems.h

---
+++ interface/L1TMuonSubsystems.h
@@ -0,0 +1,13 @@
+#ifndef DataFormats_L1TMuonPhase2_L1TMuonSubsystems_h
+#define DataFormats_L1TMuonPhase2_L1TMuonSubsystems_h
+
+// In CMSSW_11_1_X, DataFormats/L1TMuon/interface/L1TMuonSubsystems.h does not exist.
+// This is added as a temporary fix - should be removed later.
+
+namespace L1TMuon {
+
+  enum subsystem_type { kDT, kCSC, kRPC, kGEM, kME0, kNSubsystems };
+
+}  // namespace L1TMuon
+
+#endif  // DataFormats_L1TMuonPhase2_L1TMuonSubsystems_h not defined
